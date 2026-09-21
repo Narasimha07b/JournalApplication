37e3c5f8d9955db452574a62fb859e98efa3ab19
Bolla Laxminarasimha <bollanarasimha3@gmail.com>
2026-09-21 17:54:43 +0530

    Spring Security Configuration, User Authentication, and Public controller.
    Signed-off-by: Bolla Laxminarasimha <bollanarasimha3@gmail.com>

--- src/main/java/net/backend/journalApp/controller/PublicController.java
+++ src/main/java/net/backend/journalApp/controller/PublicController.java
@@ -19,7 +19,7 @@ public class PublicController {
     }
 
 
-    @PostMapping("/register")
+    @PostMapping("/create-user")
     public void createUser(@RequestBody User user){
         userService.saveNewEntry(user);
     }
